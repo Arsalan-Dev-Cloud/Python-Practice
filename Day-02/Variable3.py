1f3d907cc120d33ea138463386b830422d0fd930
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-06-29 13:34:28 +0530

    Create Variable3.py

---
+++ Day-02/Variable3.py
@@ -0,0 +1,19 @@
+print("----- Student Information -----")
+name=input("What is your name : ")
+age=input("What is your age : ")
+city=input("Which is our city : ")
+language=input("Whihch is your favourite language : ")
+
+print("Name : ",name)
+print("Age : ",age)
+print("City : ",city)
+print("Favourte Language : ",language)
+
+
+//-----------------------------------------------------//
+output:-
+----- Student Information -----
+Name : Arsalan
+Age : 22
+City : Latur
+Favorite Language : Python
