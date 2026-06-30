d0a3d014760dcc4217572d6f0aa0892195fbd124
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-06-30 12:44:58 +0530

    Add files via upload

---
+++ Day-03/typeconversation.py
@@ -0,0 +1,26 @@
+#------ String to Integer --------
+age = int(input("Enter age: "))
+
+print(age)
+print(type(age))
+
+
+#------ String to Float --------
+height = float(input("Enter height: "))
+print(height)
+
+
+#----- Integer to String --------
+age = 22
+text = str(age)
+
+print(text)
+print(type(text))
+
+
+#----- Float to String --------
+height=5.9
+text=str(height)
+
+print(text)
+print(type(text))
