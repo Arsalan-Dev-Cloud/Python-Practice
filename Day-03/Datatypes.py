15485011681e6d934d7af0fa13f3371ecbeced5c
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-06-30 12:43:55 +0530

    Add basic data types and print statements

---
+++ Day-03/Datatypes.py
@@ -0,0 +1,9 @@
+name= 'Arsalan'
+age= 22
+height= 5.9
+is_student= True
+
+print("name:",name,type(name))
+print("age:",age,type(age))
+print("height:",height,type(height))
+print("is_student:",is_student,type(is_student))
