ff8130f86997c374e338c65b5b5863729eb31de1
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-07-01 17:14:25 +0530

    Add basic arithmetic operations in challenge.py
    This program performs basic arithmetic operations including addition, subtraction, multiplication, division, floor division, modulus, and power on two user-input integers.

---
+++ Day-04_Operators/challenge.py
@@ -0,0 +1,23 @@
+#----------------Program--------------------
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of b : "))
+
+print("Addition : ", a+b)
+print("Substraction : ", a-b)
+print("Multiplication : ", a*b)
+print("Division : ", a/b)
+print("Floor Division : ", a//b)
+print("Modules : ", a%b)
+print("Power : ", a**b)
+
+
+#---------------OUTPUT----------------
+Enter the value of a : 10
+Enter the value of b : 20
+Addition :  30
+Substraction :  -10
+Multiplication :  200
+Division :  0.5
+Floor Division :  0
+Modules :  10
+Power :  100000000000000000000
