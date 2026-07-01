659f611993478980316729678da976b577538cbb
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-01 16:56:32 +0530

    Implement logical operators in logical.py
    Added logical operator examples demonstrating 'and', 'or', and 'not'.

---
+++ Day-04_Operators/logical.py
@@ -0,0 +1,12 @@
+#--------------Program----------------
+age = 22
+salary = 30000
+
+print(age > 18 and salary > 25000)
+print(age > 30 or salary > 25000)
+print(not(age > 18))
+
+#------------Output------------------
+True
+True
+False
