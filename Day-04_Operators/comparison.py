48185bd4f5356c9f8d147a10ee735a1517dcbb03
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-01 16:54:15 +0530

    Create comparison.py for operator examples
    Add comparison operators demonstration in comparison.py

---
+++ Day-04_Operators/comparison.py
@@ -0,0 +1,16 @@
+#-----------Program--------------
+a = 10
+b = 20
+
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+
+#-----------Output---------------
+False
+True
+True
+False
+True
+False
