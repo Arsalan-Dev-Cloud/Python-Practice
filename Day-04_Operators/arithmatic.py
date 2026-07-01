5b828bdff56d8c8690682be922afb86f3239a99e
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-07-01 16:44:16 +0530

    Add arithmetic operations in arithmatic.py
    This program performs basic arithmetic operations including addition, subtraction, multiplication, division, floor division, modulus, and power.

---
+++ Day-04_Operators/arithmatic.py
@@ -0,0 +1,20 @@
+#----------------- Program -----------------
+a = 20
+b = 3
+
+print("Addition : ", a+b)
+print("Substraction : ", a-b)
+print("Multiplication : ",  a*b)
+print("Division : ", a/b)
+print("Floor Division : ", a//b)
+print("Modules : ", a%b)
+print("Power : ", a ** b)
+
+#--------------- Output --------------------
+Addition :  23
+Substraction :  17
+Multiplication :  60
+Division :  6.666666666666667
+Floor Division :  6
+Modules :  2
+Power :  8000
