893e88f0c55bdaece59f45340d593505030fb729
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-02 12:17:56 +0530

    Add student profile input and string manipulations
    This script collects user input for name, city, and course, then displays the information along with string manipulations.

---
+++ Day 5 – Strings/Challange.py
@@ -0,0 +1,27 @@
+name=input("Enter Your Name:")
+city=input("Enter Your City:")
+course=input("Enter Your Course:")
+
+#=====================================
+#STUDENT PROFILE
+#=====================================
+print(f"My name is : {name}")
+print(f"I live in : {city}")
+print(f"I am learning : {course}")
+
+print(f"Upper Case : {name.upper()}")
+print(f"Lower Case : {name.lower()}")
+print(f"Characters : {len(course)}")
+#======================================
+
+
+#----------------Output------------------
+Enter Your Name:Shaikh Arsalan
+Enter Your City:Latur
+Enter Your Course:Data Engineering
+My name is : Shaikh Arsalan
+I live in : Latur
+I am learning : Data Engineering
+Upper Case : SHAIKH ARSALAN
+Lower Case : shaikh arsalan
+Characters : 16
