f918f7c839c0829dcd1db79c0ea21985246ca283
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-02 12:07:44 +0530

    Add F-string formatting example in formatting.py
    Add a program demonstrating the use of F-strings for string formatting in Python.

---
+++ Day 5 – Strings/formatting.py
@@ -0,0 +1,12 @@
+#-------------Program for F-string------------
+name = "Arsalan"
+course = "Python"
+
+print(f"My name is {name}.")
+print(f"I am learning {course}.")
+
+# F-string means formatting the string
+
+#------------Output----------------
+My name is Arsalan.
+I am learning Python.
