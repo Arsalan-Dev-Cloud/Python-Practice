ebc8664d37393deb7598126e9a6e5a63419b8662
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-02 11:49:58 +0530

    Add string initialization and print statements
    Initialize strings for name and city, and print first and last names.

---
+++ Day 5 – Strings/strings.py
@@ -0,0 +1,15 @@
+#----------------Initialization of String----------------
+name = "Arsalan"
+city = 'Latur'
+message = """Welcome to Python"""
+
+#----------------Presentation of String-------------------
+First_name = "Arsalan"
+Last_name = "Shaikh"
+
+print(First_name)
+print(Last_name)
+
+#----------------Output-------------------
+Arsalan
+Shaikh
