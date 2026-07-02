b57c1375d3f51509baddecde25f5700bd5ea28f8
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-02 11:57:53 +0530

    Implement string slicing example in slicing.py
    Added a program demonstrating string slicing in Python.

---
+++ Day 5 – Strings/slicing.py
@@ -0,0 +1,17 @@
+#----------Program for slicing of the string-------------
+text = "DataEngineering"
+
+print(text[0:4])
+print(text[4:15])
+print(text[:4])
+print(text[4:])
+print(text[-5:])
+
+# Syntax->string[start:end]
+
+#------------output------------------
+Data
+Engineering
+Data
+Engineering
+ering
