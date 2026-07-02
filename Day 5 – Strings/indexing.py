e13ecf14823bc45465e12c9f07232dab8d80410a
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-02 11:54:41 +0530

    Add string indexing examples in indexing.py
    Demonstrate string indexing in Python with examples.

---
+++ Day 5 – Strings/indexing.py
@@ -0,0 +1,15 @@
+#------------Indexing the String-----------------
+language = "python"
+
+print(language[0])
+print(language[1])
+print(language[-1])
+
+# In indexing each and every character has an indexing two terms that are +ve and -ve,
+# Where the Positivie indexing starts with 012345 and the negative indexing looks like ....-6 -5 -4 -3 -2 -1
+
+
+#----------output of above program----------------
+p
+y
+n
