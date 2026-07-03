6e8fe035446e7cd7052f5c8f15783d145176e1d6
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-07-03 21:29:48 +0530

    Add if-else statement for voting eligibility

---
+++ Day-06_conditional_statements/if_else.py
@@ -0,0 +1,10 @@
+#--------------Program for if_else Statement--------------
+age = 16
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("not eligible to vote")
+
+#-----------Output------------
+Not eligible to vote
