f23edfe7c6fc35583cdd055bda855941295e620d
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-07-03 22:15:41 +0530

    Enhance short_tips.py with shorthand and ternary examples
    Added examples of shorthand and ternary operator for age status.

---
+++ Day-06_conditional_statements/short_tips.py
@@ -0,0 +1,15 @@
+#--------------------short hand-------------------
+age = 20
+
+if age >= 18: print("Adult")
+#----------output--> Adult
+
+
+#------------------Ternary operator---------------
+#------syntax: value_if_ture if condition else value_if_false
+age = 20
+
+status = "Adult" if age >= 18 else "Minor"
+
+print(status)
+#----------output--> Adult
