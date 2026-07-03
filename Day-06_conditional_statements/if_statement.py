3a3eb3895987f3262cec8ba34d876a0029c55945
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-03 21:27:16 +0530

    Implement simple if statement for exam passing
    Added a simple if statement to check pass marks.

---
+++ Day-06_conditional_statements/if_statement.py
@@ -0,0 +1,12 @@
+#-------------Program for Simple if Statement----------------
+#--Syntax: if condition:
+                #code to execute
+#------------------------------------------------------------
+
+marks = 80
+
+if marks >= 35:
+    print("You passed the exam.")
+
+#------------Output----------------
+You passed the exam.
