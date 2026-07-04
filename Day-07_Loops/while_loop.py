6111c9d8b6d8e8031f295473abc14ef40046f838
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-04 14:32:15 +0530

    Implement while loop examples in while_loop.py
    Added a while loop example and an infinite loop example.

---
+++ Day-07_Loops/while_loop.py
@@ -0,0 +1,15 @@
+# A while loop repeats as long as a condition in true
+# syntax--> while condition:
+#               # code
+
+# Program for while loop
+count = 1
+
+while count <=5:
+    print(count)
+    count += 1   #count = count + 1
+
+
+# Infinite loop
+while True:
+    print("Hello")
