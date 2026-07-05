a59201e052baf65c0dab0e62482eb59808e6d5d0
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-07-05 13:05:00 +0530

    Add example of list functions in examples.py

---
+++ Day-08_lists/examples.py
@@ -0,0 +1,7 @@
+#------------List Functions-----------
+marks = [78,85,90,67,88]
+
+print("Highest Marks:", max(marks)) # 90
+print("Lowest Marks:", min(marks)) # 67
+print("Total Marks:", sum(marks)) # 408
+print("Average:", sum(marks)/len(marks)) # 81.6
