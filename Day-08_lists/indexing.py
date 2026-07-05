7d6d38f2dc1b55ff21e8d921c667f66e405b5f67
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-05 12:44:31 +0530

    Add list indexing example in indexing.py
    Demonstrate list indexing with fruits.

---
+++ Day-08_lists/indexing.py
@@ -0,0 +1,12 @@
+#----------List indexing---------------
+fruits = ["Apple", "Banana", "Cherry"]
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[-1])
+
+
+#------------Output---------------
+Apple
+Banana
+Cherry
