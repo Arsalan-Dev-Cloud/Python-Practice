3fef3800eacf40f1bf8476792e8d6db0a13d521b
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-05 13:03:50 +0530

    Add nested list example in nested_list.py

---
+++ Day-08_lists/nested_list.py
@@ -0,0 +1,8 @@
+#------------Nested List-----------
+# A nested list is a list that contains other lists as its elements.
+matrix = [[1,2,3],
+          [4,5,6],
+          [7,8,9]
+]
+
+print(matrix[1][2]) #----> 6
