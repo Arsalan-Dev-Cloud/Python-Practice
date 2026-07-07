a6056108bb50d2584e38d04c450c986d2ff1dc7a
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-07 17:19:52 +0530

    Implement tuple indexing example in indexing.py
    Added a program demonstrating tuple indexing and negative indexing.

---
+++ Day-09_Tuples/indexing.py
@@ -0,0 +1,15 @@
+# Tuple Indexing:
+# Apple   Banana   Mango
+#  0        1        2
+#----------------------------------
+# Tuple Negative Indexing:
+# Apple   Banana   Mango
+#  -3       -2       -1
+#----------------------------------
+
+#-----------Program For indexing of tuple-------------
+fruits = ("Apple", "Banana", "Mango")
+
+print(fruits[0]) # Apple
+print(fruits[1]) # Banana
+print(fruits[-1]) # Mango
