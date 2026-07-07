01c65afeb01b4097bd7075af0e00bcc9cf72fc9d
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-07 17:37:07 +0530

    Add example of tuple packing and unpacking
    Demonstrate tuple packing and unpacking with a student tuple.

---
+++ Day-09_Tuples/unpacking.py
@@ -0,0 +1,17 @@
+# This is tuple packing 
+student = ("Arsalan", 22, "India")
+
+
+# This is tuple unpacking
+student = ("Arsalan", 22, "India")
+
+name, age, country = student
+
+print(name)
+print(age)
+print(country)
+
+#-----------Output-----------
+Arsalan
+22
+India
