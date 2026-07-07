0254de396782d65a71c6ec9465e48bed494fe824
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-07-07 17:38:28 +0530

    Add example of tuple usage for subjects
    Added a real-world example of using tuples to list subjects.

---
+++ Day-09_Tuples/example.py
@@ -0,0 +1,17 @@
+# Real world example 
+subjects = ("Math", "Science", "English", "History", "Geography")
+
+print("Subjects Offered")
+
+for subject in subjects:
+    print(subject)
+
+
+#----------output------------
+Subjects Offered
+Math
+Science
+English
+History
+Geography
+PS E:\PYTHO
