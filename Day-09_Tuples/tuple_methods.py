130f116949650110bbffadcc8cbaaefbabe198fe
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-07 17:35:10 +0530

    Implement examples for tuple methods
    Added examples demonstrating tuple methods count() and index().

---
+++ Day-09_Tuples/tuple_methods.py
@@ -0,0 +1,11 @@
+#-----------Methos of tuple------------
+# There are only 2 inbuild methods for tuple
+
+
+# count()
+numbers = (1,2,2,3,2)
+print(numbers.count(2)) # 3
+
+# index()
+fruits = ("Apple", "Banana", "Mango")
+print(fruits.index("Banana")) # 1
