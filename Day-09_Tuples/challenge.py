31e0d4b16d012711ada641fcbab7c3361bc7ffde
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-07-07 17:47:41 +0530

    Add employee information display program
    This program displays employee information including ID, name, department, salary, and city.

---
+++ Day-09_Tuples/challenge.py
@@ -0,0 +1,18 @@
+# Challange Program: -
+employee_info = (1, "Arsalan", "Data Engineer", 60000, "Latur")
+emp_id, name, department, salary, city = employee_info
+
+print("----------- Employee Information -----------")
+print(f"ID         : {emp_id}")
+print(f"Name       : {name}")
+print(f"Department : {department}")
+print(f"Salary     : ₹{salary:,}")
+print(f"City       : {city}")
+
+#--------------Output---------------
+----------- Employee Information -----------
+ID         : 1
+Name       : Arsalan
+Department : Data Engineer
+Salary     : 60000
+City       : Latur
