4cef16828246dc5ee6772a60dd0e8780b4469038
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-12 15:07:55 +0530

    Clean up student report card logic
    Refactor student report card calculations and output.

--- Day-10_Dictionaries/challange.py
+++ Day-10_Dictionaries/challange.py
@@ -15,30 +15,13 @@ count =0
 total =0
 for key, value in student.items():
     if "Marks" in key:
-        total += valuestudent = {
-    "Name": "Arsalan",
-    "Roll Number": 60,
-    "Course": "Big Data",
-    "Maths Marks": 95,
-    "Science Marks": 90,
-    "English Marks": 85
-}
-
-print("-----------Student Report Card------------")
-for key, value in student.items():
-    print(f"{key:<20}: {value}")
-
-count =0
-total =0
-for key, value in student.items():
-    if "Marks" in key:
         total += value
         count += 1
 
 average = total/count
 
-print(f"{"Total":<20}: {total}")
-print(f"{"Avearge":<20}: {average:.2f}")
+print(f"{'Total':<20}: {total}")
+print(f"{'Avearge':<20}: {average:.2f}")
 
 grades = {
     90: "A",
@@ -50,27 +33,7 @@ grades = {
 
 for marks in grades:
     if average >= marks:
-        print(f"{"Grade":<20}: {grades[marks]}")
-        break
-        count += 1
-
-average = total/count
-
-print(f"Total  : {total}")
-print(f"Avearge: {average:.2f}")
-
-
-grades = {
-    90: "A",
-    80: "B",
-    70: "C",
-    60: "D",
-    0: "F"
-}
-
-for marks in grades:
-    if average >= marks:
-        print("Grade:", grades[marks])
+        print(f"{'Grade':<20}: {grades[marks]}")
         break
 
 # Output:
