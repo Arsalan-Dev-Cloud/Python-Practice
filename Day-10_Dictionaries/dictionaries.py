7c63af5169742ded7b5eb8f645e09acca0bf7ac9
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-07-12 13:49:19 +0530

    Create a dictionary for student information

---
+++ Day-10_Dictionaries/dictionaries.py
@@ -0,0 +1,8 @@
+#-------------- Creating Dictionaries -----------------
+student = {
+    "name": "Arsalan",
+    "age": 22,
+    "city": "Latur"
+}
+
+print(student) # {'name': 'Arsalan', 'age': 22, 'city': 'Latur'}
