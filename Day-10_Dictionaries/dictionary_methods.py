0a6cb51de709e31bc627abb22ba1a2a41c5ef741
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-12 13:57:57 +0530

    Implement dictionary methods in dictionary_methods.py
    Demonstrate various dictionary methods including adding, updating, and removing items.

---
+++ Day-10_Dictionaries/dictionary_methods.py
@@ -0,0 +1,32 @@
+#-------------- Methods for dictionary -----------------
+student = {
+    "name": "Arsalan",
+    "age": 22,
+    "city": "Latur"
+}
+
+print(student) # {'name': 'Arsalan', 'age': 22, 'city': 'Latur'}
+
+
+# Adding New Items:-
+student["course"] = "Python"
+print(student) #{'name': 'Arsalan', 'age': 22, 'city': 'Latur', 'course': 'Python'}
+
+# Updating Values:-
+student["age"] = 25
+print(student) # {'name': 'Arsalan', 'age': 25, 'city': 'Latur', 'course': 'Python'}
+
+
+# Removing Items:-
+
+# 1. pop()
+student.pop("city")
+print(student) # {'name': 'Arsalan', 'age': 25, 'course': 'Python'}
+
+# 2. del
+del student["age"]
+print(student) # {'name': 'Arsalan', 'course': 'Python'}
+
+# 3. clear() - it removes every thing.
+student.clear()
+print(student) # {}
