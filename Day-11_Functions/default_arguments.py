06979bcf4a2cf62cf69b1c271819777400d9eac7
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-14 15:57:19 +0530

    Enhance default_arguments.py with new function examples
    Added examples for keyword and variable length arguments.

--- Day-11_Functions/default_arguments.py
+++ Day-11_Functions/default_arguments.py
@@ -6,7 +6,35 @@ def greet(name="Guest"):
 greet()
 greet("Arsalan")
 
-
 # Output:- 
 # Hello, Guest
 # Hello, Arsalan
+
+
+#----------Keyword Arguments------------
+
+def student(name, age):
+    print(name)
+    print(age)
+
+student(age=22, name="Arsalan")
+
+# Output:-
+# Arsalan
+# 22
+
+
+#------------Variable length Argument-------------
+ 
+def add(*numbers):
+    print(numbers)
+
+add(10, 20, 30, 40) # (10, 20, 30, 40)
+
+def add(*numbers):
+    total = sum(numbers)
+    return total
+
+print(add(10, 20, 30))  # 60
+
+
