140584fe3f847037ebcdf48d94b37e5f651ed32a
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-07-14 15:44:56 +0530

    Add functions with parameters for greeting and student info

---
+++ Day-11_Functions/parameters.py
@@ -0,0 +1,14 @@
+#-----------Function with Parameters--------------
+def greet(name):
+    print(f"Hello, {name}")
+
+greet("Arsalan") # Hello, Arsalan
+greet("Ali") # Hello, Ali
+
+
+#-----------Multiple Parameters-------------------
+def student(name, age):
+    print(f"Name: {name}") # Name: Arsalan
+    print(f"Age: {age}") # Age: 22
+
+student("Arsalan",22)
