45e7ca7c6c31b48bdff479af555d699579fe06f3
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-07-26 23:22:25 +0530

    Created Day time Moudle.py

---
+++ Day-12_Modules/datetime_module.py
@@ -0,0 +1,10 @@
+#--------- Date Time Module--------
+from datetime import datetime
+today = datetime.now()
+print(today)
+# so this is current time 
+
+
+#----------Current Date------------
+from datetime import date
+print(date.today())
