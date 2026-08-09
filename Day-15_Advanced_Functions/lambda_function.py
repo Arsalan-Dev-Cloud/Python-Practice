b41e6aa13a3aaef2fd4883aaac624d4a55a74d3a
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-08-09 15:49:18 +0530

    lambda_function.py file created

---
+++ Day-15_Advanced_Functions/lambda_function.py
@@ -0,0 +1,18 @@
+#-----------Program for python using lamda fuction------------
+#syntax:- lambda function: expression
+
+square = lambda number: number * number
+
+print(square(5))
+
+
+# output:- 25
+
+
+#-------- Using addision---------
+
+add = lambda a, b: a + b
+
+print(add(10,20))
+
+# output:- 30
