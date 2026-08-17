19e48af9bdaaa20e79eb68990a9e2110c81b572a
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-08-17 06:38:31 +0530

    revisio 3 .py is given

---
+++ Practice/revision_03.py
@@ -0,0 +1,21 @@
+# Ask the user to Enter 5 numbers and store it in a list and then find the hightest, lowest, total and average.
+values = []
+for i in range(5):
+    value = int(input((f"Enter Number {i + 1} : ")))
+    values.append(value)
+
+total = sum(values)
+average = total / len(values)
+highest = max(values)
+lowest = min(values)
+
+print("-------------Result-------------")
+
+for i in range(5):
+    print(f"{'Number ' + str(i+1):<20}: {values[i]}")
+
+
+print(f"{'Total':<20}: {total}")
+print(f"{'Average':<20}: {average:.2f}")
+print(f"{'Highest':<20}: {highest}")
+print(f"{'Lowest':<20}: {lowest}")
