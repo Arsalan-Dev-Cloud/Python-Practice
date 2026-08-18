2562d11a5dfb1e5dcd8a0fac9a4dea0de511b3f3
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-08-18 12:44:11 +0530

    Output Added

--- Practice/revision_05.py
+++ Practice/revision_05.py
@@ -27,7 +27,7 @@ search_no = int(input("\nEnter the number to search: "))
 
 for i in range(len(numbers)):
     if search_no == numbers[i]:
-        count =+ 1
+        count += 1
         position.append(i)
 
 # ----------------OR we can USE------------------           - - so here either you can use upper for loop or lower for loop
@@ -49,3 +49,25 @@ print(f"{'Numbers':<20}: {numbers}")
 print(f"{'Number Found':<20}: {y_n}")
 print(f"{'Occurrence':<20}: {count}")
 print(f"{'Position':<20}: {position}")
+
+# Output:-
+"""
+Enter Number 1: 30
+Enter Number 2: 20
+Enter Number 3: 30
+Enter Number 4: 50
+Enter Number 5: 40
+Enter Number 6: 30
+Enter Number 7: 20
+Enter Number 8: 50
+Enter Number 9: 40
+Enter Number 10: 50
+
+Enter the number to search: 50
+
+--------------Result--------------
+Numbers             : [30, 20, 30, 50, 40, 30, 20, 50, 40, 50]
+Number Found        : Yes
+Occurrence          : 3
+Position            : [3, 7, 9]
+"""
