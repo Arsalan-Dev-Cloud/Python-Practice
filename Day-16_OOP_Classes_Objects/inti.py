4a86a865744d033aa21a21117c4851341abb705b
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-09-12 14:32:05 +0530

    Modified the init.py file

--- Day-16_OOP_Classes_Objects/inti.py
+++ Day-16_OOP_Classes_Objects/inti.py
@@ -1,5 +1,4 @@
-# Use of _init_() method in python
-
+# Use of __init__() method in python
 class Mobile:
     def __init__(self):
         print("Mobile Object Created")
@@ -7,3 +6,20 @@ class Mobile:
 mobile1 = Mobile()
 mobile2 = Mobile()
 mobile3 = Mobile()
+
+# Output:- 
+"""
+Mobile Object Created
+Mobile Object Created
+Mobile Object Created
+"""
+
+# Use of __init__() method in python with attributes
+class Mobile:
+    def __init__(self):
+        self.brand = "Poco"
+
+mobile1 = Mobile()
+print(mobile1.brand)
+
+# Output:- Poco
