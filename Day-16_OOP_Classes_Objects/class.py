a6f5f58d8cfd696acc19e66963effc3e1f16fd04
Arsalan-Dev-Cloud <shaikhmohammadarsalan5@gmail.com>
2026-09-12 14:21:39 +0530

    Day 16 Started

---
+++ Day-16_OOP_Classes_Objects/class.py
@@ -0,0 +1,20 @@
+# Class Creation.
+
+class Mobile:
+    pass
+
+mobile1 = Mobile()
+mobile2 = Mobile()
+mobile3 = Mobile()
+
+print(mobile1)
+print(mobile2)
+print(mobile3)
+
+
+# Output:-
+"""
+<__main__.Mobile object at 0x00000209157CCD70>
+<__main__.Mobile object at 0x00000209157D0A50>
+<__main__.Mobile object at 0x00000209157D0B90>
+"""
---
+++ Day-16_OOP_Classes_Objects/inti.py
@@ -0,0 +1,9 @@
+# Use of _init_() method in python
+
+class Mobile:
+    def __init__(self):
+        print("Mobile Object Created")
+
+mobile1 = Mobile()
+mobile2 = Mobile()
+mobile3 = Mobile()
